08e560ba2c7153bb089be258ee087d74918e1e9e
BanhCute <truongnga252003@gmail.com>
2024-12-12 17:05:01 +0700

    cmt

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,6 +17,7 @@ import { GenesModule } from './services/genes/genes.module';
 import { NovelModule } from './services/novel/novel.module';
 import { ConfigModule } from '@nestjs/config';
 import { ChaptersModule } from './services/chapters/chapters.module';
+import { CommentsModule } from './services/comments/comments.module';
 import appConfig from 'src/config/configuration';
 
 @Module({
@@ -43,6 +44,7 @@ import appConfig from 'src/config/configuration';
     AuthoritiesModule,
     RoleAuthoritiesModule,
     ChaptersModule,
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
